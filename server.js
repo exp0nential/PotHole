28a3f3f4f7524863add3f04d4e1141ed330b0616
John Doe <michaelcrowe@amdg.rockhursths.edu>
2017-03-21 11:24:17 -0500

    test

---
+++ server.js
@@ -0,0 +1,30 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+var http = require('http');
+var socketio = require('socket.io');
+
+var app = express();
+var server = http.createServer(app);
+var io = socketio(server);
+
+app.post('/echo', function(req, res) {
+    console.log('post /echo =' + JSON.stringify(req.body));
+    io.sockets.emit('echo', req.body);
+    res.status(201).send(req.body);
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.get('/', function (req, res) {
+    res.send('We are Eating!!!!')
+});
+app.post('/dht', function (req, res) {
+    console.log('post / = ' + JSON.stringify(req.body));
+    res.status(200).send('got it');
+});
+
+server.listen(3000, function () {
+    console.log('Example app listenting on port 3000!!!');
+});
